8362156abe2d49448d589c0dd2660a05a066d1aa
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2019-10-21 11:10:55 +0100

    remove debug statements

--- cibuildwheel/linux.py
+++ cibuildwheel/linux.py
@@ -57,11 +57,6 @@ def build(project_dir, output_dir, test_command, test_requires, test_extras, bef
             mkdir /output
             cd /project
 
-
-            echo 'AAA'
-
-            set -o
-
             {environment_exports}
 
             for PYBIN in {pybin_paths}; do
